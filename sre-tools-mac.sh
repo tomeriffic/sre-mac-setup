de4a19bca4424dfc43f74f826cc8748de9f86124
mohanvamc <vamsi.mohan@cision.com>
2023-06-23 22:54:45 +0530

    sre-mac-setup

--- sre-tools-mac.sh
+++ sre-tools-mac.sh
@@ -3,6 +3,3 @@ output_file="failed-tools.txt"
 
 # Install tools and continue on errors
 cat "$input_file" | xargs -I {} zsh -c 'brew install {} || true'
-
-# Display failed tools
-grep -vxFf <(brew list 2>/dev/null) "$input_file" > "$output_file"
