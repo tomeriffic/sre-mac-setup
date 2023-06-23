8daaa3ba86819c4d51af815eb12018587f9a9aad
mohanvamc <vamsi.mohan@cision.com>
2023-06-23 19:43:05 +0530

    sre-mac-setup

--- sre-tools-mac.sh
+++ sre-tools-mac.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 TOOLS_INSTALLED=0
+TOOLS_SKIPPED=0
 TOTAL_TOOLS=$(grep -c -v "#" sre-tools.txt)
 
 while read -r line; do
@@ -12,18 +13,19 @@ while read -r line; do
         fi
 
         eval $command
-        exit_status=$?
-        if [[ $exit_status -eq 0 ]]; then
+        if [[ $? -eq 0 ]]; then
             ((TOOLS_INSTALLED++))
         else
             echo "Error installing ${line}"
+            ((TOOLS_SKIPPED++))
         fi
     fi
 done < sre-tools.txt
 
 echo "Total tools: $TOTAL_TOOLS"
 echo "Tools installed: $TOOLS_INSTALLED"
+echo "Tools skipped: $TOOLS_SKIPPED"
 
-if [[ $TOOLS_INSTALLED -eq $TOTAL_TOOLS ]]; then
+if [[ $TOOLS_INSTALLED -gt 0 ]]; then
     brew cleanup
 fi
