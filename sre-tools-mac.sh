df0577f0bb62d7048da52da71071e2ca67a15f85
mohanvamc <vamsi.mohan@cision.com>
2023-06-23 18:52:08 +0530

    sre-mac-setup

--- sre-tools-mac.sh
+++ sre-tools-mac.sh
@@ -5,17 +5,23 @@ EXIT_CODE=0
 while read -r line; do
     if [[ $line != "#"* && $line != "" ]]; then
         if [[ $line == "--cask"* ]]; then
-            brew install --cask "${line/--cask /}"
+            command="brew install --cask ${line/--cask /}"
         else
-            brew install "${line}"
+            command="brew install ${line}"
         fi
 
+        output=$($command 2>&1)
         if [[ $? -ne 0 ]]; then
-            echo "Error installing ${line}"
+            echo "Error installing ${line}:"
+            echo "$output"
             EXIT_CODE=1
         fi
     fi
 done < sre-tools.txt
 
 exit $EXIT_CODE
+
+if [[ $EXIT_CODE -ne 0 ]]; then
+    echo "Script failed with an error"
+fi
 
