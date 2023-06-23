fbcebf66852dee77285bc7b2cae7c71e73ada924
mohanvamc <vamsi.mohan@cision.com>
2023-06-23 17:52:52 +0530

    sre-mac-setup

--- sre-tools-mac.sh
+++ sre-tools-mac.sh
@@ -1,12 +1,21 @@
 #!/bin/zsh
 
+EXIT_CODE=0
+
 while read -r line; do
     if [[ $line != "#"* && $line != "" ]]; then
         if [[ $line == "--cask"* ]]; then
-            brew install --cask "${line/--cask /}" || true
+            brew install --cask "${line/--cask /}"
         else
-            brew install "${line}" || true
+            brew install "${line}"
+        fi
+
+        if [[ $? -ne 0 ]]; then
+            echo "Error installing ${line}"
+            EXIT_CODE=1
         fi
     fi
 done < sre-tools.txt
+
+exit $EXIT_CODE
 
