d618858646869bcb2014b971fabd30444288184d
mohanvamc <vamsi.mohan@cision.com>
2023-06-23 22:22:15 +0530

    sre-mac-setup

--- sre-tools-mac.sh
+++ sre-tools-mac.sh
@@ -1,9 +1,14 @@
 #!/bin/zsh
 
 tool_list_file="sre-tools.txt"
+failed_tools=""
 
 while IFS= read -r tool; do
-  command="brew install $tool || true"
-
-  eval $command
+  if ! brew install "$tool"; then
+    failed_tools+="\n- $tool"
+  fi
 done < "$tool_list_file"
+
+echo -e "Failed to install the following tools:$failed_tools"
+
+brew cleanup || true
