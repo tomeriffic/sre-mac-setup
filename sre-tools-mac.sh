2862eeef96e473f83cf075119be86890fe933643
mohanvamc <vamsi.mohan@cision.com>
2023-06-23 20:35:44 +0530

    sre-mac-setup

--- sre-tools-mac.sh
+++ sre-tools-mac.sh
@@ -2,7 +2,7 @@
 
 TOOLS_INSTALLED=0
 TOOLS_SKIPPED=0
-TOTAL_TOOLS=$(grep -c -v "#" sre-tools.txt)
+SKIPPED_TOOLS=""
 
 while read -r line; do
     if [[ $line != "#"* && $line != "" ]]; then
@@ -18,13 +18,14 @@ while read -r line; do
         else
             echo "Error installing ${line}"
             ((TOOLS_SKIPPED++))
+            SKIPPED_TOOLS+="- ${line}\n"
         fi
     fi
 done < sre-tools.txt
 
-echo "Total tools: $TOTAL_TOOLS"
+echo "Total tools: $((TOOLS_INSTALLED + TOOLS_SKIPPED))"
 echo "Tools installed: $TOOLS_INSTALLED"
-echo "Tools skipped: $TOOLS_SKIPPED"
+echo -e "Tools skipped: $TOOLS_SKIPPED\n$SKIPPED_TOOLS"
 
 if [[ $TOOLS_INSTALLED -gt 0 ]]; then
     brew cleanup
